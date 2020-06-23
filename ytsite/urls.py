24203ad7bc091eca65cf3aeaf6529682b0cd3e13
noob <sharmakhushal78@gmail.com>
2020-06-23 14:21:15 +0530

    added about page

--- ytsite/urls.py
+++ ytsite/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('upload/', views.create_form, name='upload'),
     path('created/', views.created, name = "created"),
     path('comptech/', views.comptech, name = "comptech"),
--- ytsite/views.py
+++ ytsite/views.py
@@ -6,6 +6,9 @@ from .models import form_submission
 def index(request):
 	return render(request, "ytsite/playlist.html")
 
+def about(request):
+    return render(request, "ytsite/about.html")
+
 def create_form(request):
 	return render(request, "ytsite/upload.html")
 # Create your views here. 
@@ -34,20 +37,3 @@ def comptech(request):
 def tricktech(request):
     usr = form_submission.objects.all()
     return render(request, "ytsite/trick_tech.html", {'usr' : usr})
-
-
-    """if(p.playlist=="Comp_tech"):
-        print("ghus gaya")
-        q = form_submission.objects.all().filter(playlist="Comp_tech")
-        return render(request, "Comp_tech.html", {'q' : q})
-
-    elif(p.playlist=="Mech_tech") :
-        q = form_submission.objects.all().filter(playlist="Mech_tech")
-        return render(request, "Mech_tech.html", {'q' : q})
-
-    elif(p.playlist=="Comp_tech") :
-        q = form_submission.objects.all().filter(playlist="Trick_tech")
-        return render(request, "Trick_tech.html", {'q' : q})
-
-    else :
-        return render(request, "index.html", {'usr' : usr})"""
